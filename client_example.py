bf3b4c827b02ee9651fdc3053c88703ac0bf1efe
Josiah Daniels Herrera <josiahdanielsherrera@Josiahs-Air-31.home>
2024-11-17 20:45:23 -0800

    more

--- client_example.py
+++ client_example.py
@@ -3,11 +3,11 @@ import requests
 # URL for the local Flask server
 url = 'http://127.0.0.1:50000/receive_message'
 
-# JSON payload with the message
-payload = {"numdber": "19"}
+# JSON payload with the number to receive prime factorization for
+payload = {"number": "19"}
 
-# Send a POST request
+# Send an http POST request and receive the response - i.e. an example call
 response = requests.post(url, json=payload)
 
-# Print the response from the server
+# Do something with the response
 print("Server response:", response.json(), "Status Code:", response.status_code)
